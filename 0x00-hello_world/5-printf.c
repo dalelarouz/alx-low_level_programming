71a81e90449b59bbf931a569769dd31132803c3d
dalelarouz <dukedom2006@gmail.com>
2022-10-13 20:10:09 -0700

    task 8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * Main - entry
- * Return: non 0
+ * Return: always 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+
 }
